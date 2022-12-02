ecdd01eb4d5dd5a16977dd9af553bf454d100fcd
jakkosdev <45759112+jakkosdev@users.noreply.github.com>
2022-12-02 21:06:52 +0000

    use u32 instead of usize

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -18,18 +18,18 @@ fn main() {
     let num_list_per_elf = string_list_per_elf.map(|string_list| {
         string_list
             .iter()
-            .map(|string| string.parse::<usize>().unwrap())
+            .map(|string| string.parse::<u32>().unwrap())
     });
 
     // sum each list of numbers
-    let total_per_elf = num_list_per_elf.map(|num_list| num_list.sum::<usize>());
+    let total_per_elf = num_list_per_elf.map(|num_list| num_list.sum::<u32>());
 
     // sort the totals
     let mut totals_sorted_desc = total_per_elf.collect::<Vec<_>>();
     totals_sorted_desc.sort_by(|a, b| a.cmp(b).reverse());
 
     let top_total = totals_sorted_desc.first().unwrap();
-    let top_3_combined_total = totals_sorted_desc.iter().take(3).sum::<usize>();
+    let top_3_combined_total = totals_sorted_desc.iter().take(3).sum::<u32>();
 
     println!("Day 1 - answer a: {top_total}, answer b: {top_3_combined_total}")
 }
