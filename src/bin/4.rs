2aaf9c7cd63538662623f08a1cded6105370bae4
jakkosdev <45759112+jakkosdev@users.noreply.github.com>
2022-12-04 12:17:18 +0000

    fmt

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{self, BufRead}
+    io::{self, BufRead},
 };
 
 fn main() {
@@ -12,20 +12,19 @@ fn main() {
         .collect::<Vec<_>>();
 
     // create an iteartor of ((first_elf_start_section, first_elf_stop_section), (second_elf_start_section, second_elf_stop_section))
-    let elf_pair_sections = lines
-        .iter()
-        .map(|line| {
-            let mut elf_pair = line.split(",").map(|sections| {
-                let mut sec_nums = sections
-                    .split("-")
-                    .map(|section| section.parse::<u32>().unwrap());
-                (sec_nums.next().unwrap(), sec_nums.next().unwrap())
-            });
-
-            (elf_pair.next().unwrap(), elf_pair.next().unwrap())
+    let elf_pair_sections = lines.iter().map(|line| {
+        let mut elf_pair = line.split(",").map(|sections| {
+            let mut sec_nums = sections
+                .split("-")
+                .map(|section| section.parse::<u32>().unwrap());
+            (sec_nums.next().unwrap(), sec_nums.next().unwrap())
         });
 
-    let a_count = elf_pair_sections.clone()
+        (elf_pair.next().unwrap(), elf_pair.next().unwrap())
+    });
+
+    let a_count = elf_pair_sections
+        .clone()
         // filter this iterator to only contain pairs where one entirely encompasses another
         .filter(|((a_start, a_end), (b_start, b_end))| {
             (a_start <= b_start && a_end >= b_end) || (b_start <= a_start && b_end >= a_end)
@@ -34,7 +33,7 @@ fn main() {
 
     let b_count = elf_pair_sections
         // filter if the start of one elf occurs between the start and end of the other
-        .filter(|((a_start, a_end), (b_start, b_end))|{
+        .filter(|((a_start, a_end), (b_start, b_end))| {
             (a_start <= b_start && b_start <= a_end) || (b_start <= a_start && a_start <= b_end)
         })
         .count();
