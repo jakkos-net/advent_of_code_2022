adb88bafa956e92897e604e6fea6362476a4da93
jakkosdev <45759112+jakkosdev@users.noreply.github.com>
2022-12-06 08:35:50 +0000

    skeleton for day 6

---
+++ src/bin/6.rs
@@ -0,0 +1,16 @@
+
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+fn main() {
+    // get input file lines
+    let file = File::open("inputs/6.txt").unwrap();
+    let lines = io::BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<_>>();
+
+}
+
